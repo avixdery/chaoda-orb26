72e6b2019b8a1c006ba4d24a6f5ac153d169659f
Jia Ying <52987922+avixdery@users.noreply.github.com>
2026-06-26 22:15:36 +0800

    my timer is text
    it works now

---
+++ Assets/Timer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        { 
+            remainingTime = 0;
+            timerText.color = Color.red;
+            // add "time's up" visual
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text= string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
